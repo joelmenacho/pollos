2a71d04e41fb0ddf71bc634c9db937c3195ec220
joelmenacho <joelmenacho@gmail.com>
2025-11-03 07:30:23 -0500

    Update frontend dashboard y Clean Android/iOS repo; ignore keystores and backups

---
+++ frontend/src/presentation/screens/OrdersScreen.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+export default function OrdersScreen() {
+  return (
+    <View style={{ flex:1, alignItems:'center', justifyContent:'center' }}>
+      <Text>Mis pedidos</Text>
+    </View>
+  );
+}
---
+++ frontend/src/presentation/screens/UserDashboardScreen.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+export default function UserDashboardScreen() {
+  return (
+    <View style={{ flex:1, alignItems:'center', justifyContent:'center' }}>
+      <Text>Dashboard de Usuario</Text>
+    </View>
+  );
+}
